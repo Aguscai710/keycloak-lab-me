83b9b8d1600e8238d98d6658a03ffce995105e05
Aguscai710 <agustinscianfortewk@gmail.com>
2023-12-01 16:41:32 -0300

    Primer ISSUE

--- keycloak-custom-authenticator/src/main/java/com/identicum/keycloak/CustomAuthenticator.java
+++ keycloak-custom-authenticator/src/main/java/com/identicum/keycloak/CustomAuthenticator.java
@@ -1,19 +1,47 @@
 package com.identicum.keycloak;
 
+import static org.keycloak.authentication.authenticators.browser.AbstractUsernameFormAuthenticator.logger;
+
+import java.util.Random;
+
 import javax.ws.rs.core.MultivaluedMap;
 
+import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
 import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.protocol.oidc.OIDCLoginProtocol;
+import org.keycloak.services.managers.AuthenticationManager;
 
 public class CustomAuthenticator extends UsernamePasswordForm {
 
 	@Override
 	public void authenticate(AuthenticationFlowContext context) {
-		// Generar codigo random y mostrarlo en el theme
-		LoginFormsProvider form = context.form();
+		MultivaluedMap<String, String> formData = new MultivaluedMapImpl<>();
+		String loginHint = context.getAuthenticationSession().getClientNote(OIDCLoginProtocol.LOGIN_HINT_PARAM);
+		String rememberMeUsername = AuthenticationManager.getRememberMeUsername(context.getRealm(), context.getHttpRequest().getHttpHeaders());
+		Random token =  new Random();
+		if (context.getUser() != null) {
+            LoginFormsProvider form = context.form();
+            form.setAttribute(LoginFormsProvider.USERNAME_HIDDEN, true);
+            form.setAttribute(LoginFormsProvider.REGISTRATION_DISABLED, true);
+            context.getAuthenticationSession().setAuthNote(USER_SET_BEFORE_USERNAME_PASSWORD_AUTH, "true");
+        } else {
+            context.getAuthenticationSession().removeAuthNote(USER_SET_BEFORE_USERNAME_PASSWORD_AUTH);
+            if (loginHint != null || rememberMeUsername != null) {
+                if (loginHint != null) {
+                    formData.add(AuthenticationManager.FORM_USERNAME, loginHint);
+                } else {
+                    formData.add(AuthenticationManager.FORM_USERNAME, rememberMeUsername);
+                    formData.add("rememberMe", "on");
+                }
+            }
+        }
+        Response challengeResponse = challenge(context, formData);
+        context.challenge(challengeResponse);
+	}
 
-		super.authenticate(context);
+		
 	}
 
 	@Override
